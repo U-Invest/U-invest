753efdb572451ad6131e48cb45d7ce7b4e4dfde5
Matheus Ramos de Pierro <matheus.r.pierro@ericssoninovacao.com.br>
2022-11-01 17:05:59 -0300

    Criando estrutura dos projetos de Java e Web development para entrega da sprint com criação dos principais arquivos e classes entregues na primeira Sprint. foi apenas feito a criação dos arquivos, nesse caso precisamos popular os arquivos ainda com os requisitos da sprint

---
+++ U_invest/src/br/com/U_invest/beans/Conteudo.java
@@ -0,0 +1,4 @@
+package br.com.U_invest.beans;
+
+public class Conteudo {
+}
---
+++ U_invest/src/br/com/U_invest/teste/Main.java
@@ -0,0 +1,7 @@
+package br.com.U_invest.teste;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
