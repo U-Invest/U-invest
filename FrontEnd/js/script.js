3d499572da362e011fa11ff4ab5e96bb0061bc59
matheusPierro <matpierro570@gmail.com>
2023-02-13 03:41:05 -0300

    finalização do modal de login e cadastro sem a validação do formulário. Adicionei o footer e criei um sistema de envio de emailna página de contato. dei uma arrumada no headere começei a migrar a página de notícias que está dando bastante conflito. tentei brincar com java um pouco mas acabei desistindo e apagando uns códigos

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -28,7 +28,7 @@ function lightMode() {
 
 const signUpBtnLink = document.querySelector(".signUpBtn-link");
 const signInBtnLink = document.querySelector(".signInBtn-link");
-const wrapper = document.querySelector(".wrapper");
+const wrapper = document.querySelector(".wrapperLogin");
 
 signUpBtnLink.addEventListener("click", () => {
   wrapper.classList.toggle("active");
@@ -37,3 +37,70 @@ signUpBtnLink.addEventListener("click", () => {
 signInBtnLink.addEventListener("click", () => {
   wrapper.classList.toggle("active");
 });
+
+// let username = document.getElementById("username");
+// let email = document.getElementById("email");
+// let password = document.getElementById("password");
+// let terms = document.getElementById("terms");
+
+// function cadastraUsuario() {
+//   if (username && email && password && terms) {
+//     let listaUser = JSON.parse(localStorage.getItem("listaUser") || "[]");
+
+//     listaUser.push({
+//       usernameCad: username.value,
+//       emailCad: email.value,
+//       passwordCad: password.value,
+//       termsCad: terms.value,
+//     });
+
+//     localStorage.setItem("listaUser", JSON.stringify(listaUser));
+//   } else {
+//     console.log("Algo deu errado");
+//   }
+// }
+
+// let usernameDefined = document.getElementById("usernameDefined");
+// let passwordDefined = document.getElementById("passwordDefined");
+
+// function logarUsuario() {
+//   let listaUser = [];
+
+//   let userValid = {
+//     username: "",
+//     password: "",
+//   };
+
+//   listaUser = JSON.parse(localStorage.getItem("listaUser"));
+
+//   listaUser.forEach((item) => {
+//     if (
+//       usernameDefined.value == item.usernameCad &&
+//       passwordDefined.value == item.passwordCad
+//     ) {
+//       userValid = {
+//         username: item.usernameCad,
+//         password: item.passwordCad,
+//       };
+//     } else {
+//       console.log("Deu erro aqui (Primeiro If)");
+//       console.log(usernameDefined.value, item.usernameCad);
+//       console.log(usernameDefined.value, item.usernameCad);
+//     }
+//   });
+
+//   if (
+//     usernameDefined.value == userValid.username &&
+//     passwordDefined.value == userValid.password
+//   ) {
+//     let mathRandom = Math.random().toString(16).substr(2);
+//     let token = mathRandom + mathRandom;
+//     localStorage.setItem("token", token);
+//     localStorage.setItem("userLogado", JSON.stringify(userValid));
+//     console.log("Você logou! Sua sessão foi criada com sucesso.");
+//     window.alert("Você logou! Sua sessão foi criada com sucesso.");
+//   } else {
+//     console.log("Usuário ou senha incorretos");
+//     window.alert("Usuário ou senha incorretos");
+//   }
+// }
