5bbdb0a0bc60270d542293d8c2c804a3f11da002
matheusPierro <matpierro570@gmail.com>
2023-02-18 14:26:16 -0300

    Serviço de administração da plataforma em python e adicionando projeto java com maven

--- U-invest-py/admin/controller.py
+++ U-invest-py/admin/controller.py
@@ -19,15 +19,19 @@ ano = data.year
 
 def menu():
     print('====== <<< ''\033[1;96m''U-Invest''\033[0;0m'' >>> ======')
-    print('|  [''\033[1;36m''1''\033[0;0m''] Cadastrar Cliente     |')
-    print('|  [''\033[1;36m''2''\033[0;0m''] Dados do Cliente      |')
-    print('|  [''\033[1;36m''3''\033[0;0m''] Mostrar Clientes      |')
+    print(
+        '|  [''\033[1;36m''1''\033[0;0m''] Cadastrar Usuário Cadastrado com sucesso!     |')
+    print(
+        '|  [''\033[1;36m''2''\033[0;0m''] Dados do Usuário Cadastrado com sucesso!      |')
+    print(
+        '|  [''\033[1;36m''3''\033[0;0m''] Mostrar Usuário Cadastrado com sucesso!s      |')
     print('|  [''\033[1;36m''4''\033[0;0m''] Gerar Relatório       |')
-    print('|  [''\033[1;36m''5''\033[0;0m''] Remover Cliente       |')
+    print(
+        '|  [''\033[1;36m''5''\033[0;0m''] Remover Usuário Cadastrado com sucesso!       |')
     print('|  [''\033[1;36m''0''\033[0;0m''] Sair                  |')
-    print('--------------------------------')
+    print('------------------------------')
     x = input('\033[1;36m''Insira a opção: ''\033[0;0m')
-    print('--------------------------------')
+    print('------------------------------')
     return x
 
 
@@ -49,7 +53,7 @@ def cadastro():
             # Confere se o login cadastrado é igual ao login da linha
             limpaTerminal()
             criaBarra()
-            print('\033[1;31m''Login já existente!''\033[0;0m')
+            print('\033[1;31m''Login ja existente!''\033[0;0m')
             criaBarra()
             return
     lerLogins.close()
@@ -58,3 +62,60 @@ def cadastro():
     senha = service.Senha()
     email = service.Email()
     data = service.Data()
+    celular = service.Celular()
+    endereco = service.Endereco()
+
+    limpaTerminal()
+    criaBarra()
+    print('\033[1;32m''Usuario Cadastrado com sucesso!''\033[0;0m')
+    criaBarra()
+
+    # --> Adiciona usuário no banco de dados usuarios.txt
+    logins = open('usuarios.txt', 'a')
+    logins.write(
+        f'Nome: {nome} -Login: {login} -Senha: {senha} -Email: {email} -Data de Nascimento: {data} -Numero de Celular: {celular} -Endereco: {endereco} \n')
+    logins.close()
+    return
+
+
+''' Logar um usuário e printar seus dados cadastrados '''
+
+
+def mostraDados():
+    limpaTerminal()
+    print('=== << ''\033[1; 33m''Dados do Usuario''\033[0;0m'' >> ===')
+    criaBarra()
+    print('\033[1;33m''Logue para acessar seus dados!''\033[0; 0m')
+    criaBarra()
+    userLogin = input('Login: ')
+    userSenha = input('Senha: ')
+
+    # Variavel de validação do login
+    valida = False
+
+    logins = open('usuarios.txt', 'r')
+    for linha in logins. readlines():
+        valores = linha.split('-')
+        if userLogin == valores[1].split(':')[1].strip() and userSenha in valores[2].split(':')[1].strip():
+            limpaTerminal()
+            criaBarra()
+            print('\033[1;32m''Usuario Logado! Dados do usuário: ''\033[0; 0m')
+            criaBarra()
+            for percorre in range(len(valores)):
+                if valores[percorre].split(': ')[0] == 'Endereco':
+                    dictEndereco = eval(
+                        valores[percorre].split('Endereco:')[1])
+                    for chave in dictEndereco:
+                        print(f'{chave}: {dictEndereco [chave]}')
+                else:
+                    print(valores[percorre])
+            criaBarra()
+            valida = True
+            logins.close()
+            break
+
+    if not valida:
+        limpaTerminal()
+        criaBarra()
+        print('\033[1;31m''Erro! Login ou senha invalidos''\033[0; 0m')
+        criaBarra()
