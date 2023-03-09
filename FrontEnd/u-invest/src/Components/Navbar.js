04b378b57e4d7ff07fef1609ace8a7fa80df95d5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-09 20:30:36 -0300

    Adicionando no assets a logo do projeto

--- FrontEnd/u-invest/src/Components/Navbar.js
+++ FrontEnd/u-invest/src/Components/Navbar.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
-import Logo from "../Assets/Logo.svg";
+import Logo from "../Assets/logo-transparente.png";
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import Box from "@mui/material/Box";
@@ -44,7 +44,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className="nav-logo-container">
-        <img src={Logo} alt="" />
+        <img src={Logo} alt=""/>
       </div>
       <div className="navbar-links-container">
         <a href="">Home</a>
