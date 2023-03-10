ed36e6bf9660814df54fead58e4a4831cc623d16
Gabriel-Triceri <rm97121@fiap.com.br>
2023-03-10 17:23:37 -0300

    <Arrumando commits com erro>

--- Database/U-invest.sql V2.sql
+++ Database/U-invest.sql V2.sql
@@ -62,28 +62,28 @@ create table curso (
     recompensa number(3)
 );
 select * from curso
-DELETE FROM nome_tabela;
+DELETE FROM curso;
 
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (10, 8, 'aprenda os fundamentos do javascript', 'joão da silva', 'javascript básico', 'jsb1', 50);
+values (10, 8, 'aprenda os fundamentos do javascript', 'joão da silva', 'javascript básico', 'jsb1', 50); 1
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (15, 9, 'aprenda a construir websites com html e css', 'maria souza', 'html e css essencial', 'htmlec1', 75);
+values (15, 9, 'aprenda a construir websites', 'maria souza', 'html e css essencial', 'html', 75); 2
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (20, 7, 'aprenda a criar aplicações web dinâmicas com php', 'pedro oliveira', 'php para iniciantes', 'phpi1', 100);
+values (20, 7, 'aprenda a criar aplicações web dinâmicas com php', 'pedro oliveira', 'php para iniciantes', 'php', 100); 3
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (5, 10, 'aprenda a usar ferramentas de análise de dados com excel', 'ana santos', 'excel avançado', 'exa1', 150);
+values (5, 10, 'aprenda a usar ferramentas de análise de dados com excel', 'ana santos', 'excel avançado', 'exa1', 150); 4
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (30, 8, 'aprenda a desenvolver aplicações móveis para android', 'rafael costa', 'android app development', 'aad1', 200);
+values (30, 8, 'aprenda a desenvolver aplicações móveis para android', 'rafael costa', 'android app development', 'aad1', 200); 5
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (25, 6, 'aprenda a programar em python para análise de dados', 'juliana fernandes', 'python para análise de dados', 'pyad1', 125);
+values (25, 6, 'aprenda a programar em python para análise de dados', 'juliana fernandes', 'python para análise de dados', 'pyad', 125); 6
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (12, 9, 'aprenda a criar interfaces gráficas com javafx', 'lucas mendes', 'javafx para desenvolvedores', 'jfx1', 175);
+values (12, 9, 'aprenda a criar interfaces gráficas com javafx', 'lucas mendes', 'javafx para desenvolvedores', 'jfx1', 175); 7
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (8, 7, 'aprenda a criar jogos com a unity', 'felipe santos', 'desenvolvimento de jogos com unity', 'unity1', 100);
+values (8, 7, 'aprenda a criar jogos com a unity', 'felipe santos', 'desenvolvimento de jogos com unity', 'unit', 100); 8
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (18, 8, 'aprenda a usar o react para criar aplicações web', 'gabriel almeida', 'react para desenvolvedores web', 'react1', 150);
+values (18, 8, 'aprenda a usar o JS para criar aplicações web', 'gabriel almeida', 'JS para desenvolvedores web', 'JS', 150); 
 insert into curso (duracao, avaliacao, resumo, nome_professor, nome, id_curso, recompensa)
-values (7, 10, 'aprenda a usar ferramentas de edição de vídeo com adobe premiere', 'paulo roberto', 'edição de vídeo com adobe premiere', 'prm1', 75);
+values (7, 10, 'ferramentas de edição de vídeo, adobe premiere', 'paulo roberto', 'edição de vídeo com premiere', 'prm1', 75); 10
 
 create table modulo (
     resumo varchar(100),
@@ -91,12 +91,13 @@ create table modulo (
     nome varchar(30)
 );
 
+select * from modulo
 insert into modulo (resumo, id_modulo, nome)
 values ('Introdução à programação', 'mod001', 'Programação básica');
 insert into modulo (resumo, id_modulo, nome)
-values ('Estrutura de dados', 'mod002', 'Algoritmos e estruturas de dados');
+values ('Estrutura de dados', 'mod002', 'Estruturas de dados');
 insert into modulo (resumo, id_modulo, nome)
-values ('Desenvolvimento web', 'mod003', 'Front-end com HTML, CSS e JavaScript');
+values ('Desenvolvimento web', 'mod003', 'Front-end com HTML e CSS');
 insert into modulo (resumo, id_modulo, nome)
 values ('Lógica de programação', 'mod004', 'Lógica para iniciantes');
 insert into modulo (resumo, id_modulo, nome)
@@ -120,7 +121,8 @@ create table aula(
     contudo char(20),
     avaliacao number(2)
 );
-
+desc aula
+select * from aula
 insert into aula (tipo_contendo, duracao, nome_aula, id_aula, conteudo, avaliacao)
 values ('Vídeo', '2022-03-01', 'Introdução ao curso', 'aula001', 'Apresentação do curso', 90);
 insert into aula (tipo_contendo, duracao, nome_aula, id_aula, conteudo, avaliacao)
@@ -153,7 +155,7 @@ id_certificado char(8) constraint cert_id_pk primary key,
 nome_professor char(30));
 
 insert into certificado (nome_curso, nome_aluno, assinatura_plat, carga_horaria, dt_emissao, dt_conclusao, id_certificado, nome_professor)
-values ('Introdução à Programação', 'João da Silva', 'Digital Signature Inc.', '30h', '2022-03-05', '2022-03-01', 'cert001', 'Maria Souza');
+values ('Introdução à Programação', 'João da Silva', 'Digital Signature', '30h', TO_DATE('08/06/2022', 'DD/MM/YYYY'), TO_DATE('09/07/2022', 'DD/MM/YYYY'), 'cert001', 'Maria Souza');
 insert into certificado (nome_curso, nome_aluno, assinatura_plat, carga_horaria, dt_emissao, dt_conclusao, id_certificado, nome_professor)
 values ('Python para Iniciantes', 'Ana Santos', 'CertSign', '40h', '2022-03-10', '2022-03-06', 'cert002', 'Paulo Oliveira');
 insert into certificado (nome_curso, nome_aluno, assinatura_plat, carga_horaria, dt_emissao, dt_conclusao, id_certificado, nome_professor)
@@ -176,27 +178,28 @@ create table login (
     ultima_sessao date,
     hr_sessao_atual date
 );
+SELECT value FROM nls_session_parameters WHERE parameter = 'NLS_DATE_FORMAT';
+SELECT value FROM nls_session_parameters WHERE parameter = 'NLS_TIME_FORMAT';
 
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('12345678', 10, 'Introdução ao SQL', '2022-02-28', '2022-02-28 15:30:00');
+VALUES ('12345678', 10, 'Introdução ao SQL', '10/03/23', '2022-02-28 15:30:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('87654321', 20, 'Operações com Tabelas', '2022-03-01', '2022-03-01 09:45:00');
+VALUES ('87654321', 20, 'Operações com Tabelas', '14/04/23', '2022-03-01 09:45:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('45678901', 5, 'Funções de Agregação', '2022-03-02', '2022-03-02 13:15:00');
+VALUES ('45678901', 5, 'Funções de Agregação', '22/07/22', '2022-03-02 13:15:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('78901234', 12, 'Joins em SQL', '2022-03-03', '2022-03-03 10:00:00');
+VALUES ('78901234', 12, 'Joins em SQL', '20/03/21', '2022-03-03 10:00:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('23456789', 8, 'Subconsultas', '2022-03-04', '2022-03-04 14:20:00');
+VALUES ('23456789', 8, 'Subconsultas', '06/03/21', '2022-03-04 14:20:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('34567890', 15, 'Indexação em SQL', '2022-03-05', '2022-03-05 11:30:00');
+VALUES ('34567890', 15, 'Indexação em SQL', '10/12/21', '2022-03-05 11:30:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('90123456', 7, 'Stored Procedures', '2022-03-06', '2022-03-06 16:00:00');
+VALUES ('90123456', 7, 'Stored Procedures', '09/02/20', '2022-03-06 16:00:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('67890123', 18, 'Transações em SQL', '2022-03-07', '2022-03-07 12:45:00');
+VALUES ('67890123', 18, 'Transações em SQL', '03/02/17', '2022-03-07 12:45:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('01234567', 3, 'Backup e Restore', '2022-03-08', '2022-03-08 14:10:00');
+VALUES ('01234567', 3, 'Backup e Restore', '03/07/21', '2022-03-08 14:10:00');
 INSERT INTO login (id_sessao, hrs_plataforma, nome_aula, ultima_sessao, hr_sessao_atual)
-VALUES ('76543210', 22, 'Análise de Dados com SQL', '2022-03-09', '2022-03-09 09:30:00');
-
-
+VALUES ('76543210', 22, 'Análise de Dados com SQL','07/12/2022','09:30:00');
+dd
 commit;
