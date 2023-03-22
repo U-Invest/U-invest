65e86d344373906c0086232da7d0ce1bac3c80f3
Thiago Castro <thiagogyorgy.castro@gmail.com>
2023-03-21 23:12:34 -0300

    Fazendo novo front-end com react, e renomeando pasta antiga

---
+++ UInvest-FrontEnd/src/components/index.js
@@ -0,0 +1,9 @@
+import Home from "./container/Home";
+import About from "./container/About";
+import Teacher from "./container/Teacher"
+import Courses from "./container/Course/Courses";
+import Contact from "./container/Contact";
+import Footer from "./container/Footer";
+import Navbar from "./Navbar/Navbar";
+
+export { Navbar, Home, About, Courses, Teacher,Contact, Footer };
