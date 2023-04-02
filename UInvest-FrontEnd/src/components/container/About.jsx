30274e65b3e63abb8e18a24bb697dd57fe54cec0
Thiago Castro <thiagogyorgy.castro@gmail.com>
2023-04-02 10:14:43 -0300

    Arrumando erros ortográficos

--- UInvest-FrontEnd/src/components/container/About.jsx
+++ UInvest-FrontEnd/src/components/container/About.jsx
@@ -11,10 +11,10 @@ const About = () => {
         <div>
           <div className="font-bold sm:text-[1.875rem] text-[1.5rem] mb-5">
             Vamos proporcionar os<br /> melhores{" "}
-            <span className="text-Teal">cursos onlines!</span>
+            <span className="text-Teal">cursos online!</span>
           </div>
           <p className="text-sm text-gray leading-7 mb-4">
-            A U Invest chegou no mercado para mudar o cenário de investimento!, com uma proposta inovadora unindo tecnnologia e investimento, com a proposta de multiplicar os acionistas brasileiros, venha nos conhecer melhor! Veja nossos cursos do catálogo ou até mesmo converse com o “Droid” nosso chatbot inteligente pronto pra sanar as suas dúvidas, e te ensinar muito, vale a pena ver. Eai, Bora Investir?
+            A U Invest chegou no mercado para mudar o cenário de investimento. Com uma proposta inovadora unindo tecnnologia e investimento, com a proposta de multiplicar os acionistas brasileiros, venha nos conhecer melhor! Veja nossos cursos do catálogo ou até mesmo converse com o “Droid” nosso chatbot inteligente pronto pra sanar as suas dúvidas, e te ensinar muito, vale a pena ver. Eai, Bora Investir?
           </p>
           <button className="py-3 px-6 text-sm border border-solid border-gray rounded-lg font-bold">
             Saiba mais
