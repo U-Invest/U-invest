fc358825b335bb169dc0854ca2bb12a968c7629c
Gabriel-Triceri <rm97121@fiap.com.br>
2023-05-18 20:38:50 -0300

    Droid

--- Droid/droid.py
+++ Droid/droid.py
@@ -1,6 +1,8 @@
 #nltk: Biblioteca para processamento de linguagem natural. Pode ser instalada usando o comando pip install nltk.
 #scikit-learn: Biblioteca para aprendizado de máquina e processamento de dados. Pode ser instalada usando o comando pip install scikit-learn.
 import nltk
+import textwrap
+import webbrowser
 from nltk.chat.util import Chat, reflections
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
@@ -8,8 +10,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.tokenize import sent_tokenize
 from heapq import nlargest
-nltk.download('stopwords')#Caso seja nessesario
-nltk.download('punkt')    #Caso seja nessesario
+#nltk.download('stopwords')#Caso seja nessesario
+#nltk.download('punkt')    #Caso seja nessesario
 
 stemmer = SnowballStemmer('portuguese')
 stop_words = stopwords.words('portuguese')
@@ -30,13 +32,10 @@ print("-" * 89 + "\033[m")
 def preprocess(text):
     # Tokenização
     tokens = nltk.word_tokenize(text.lower())
-
     # Remoção de stop words
     tokens = [token for token in tokens if token not in stop_words]
-
     # Stemming
     stemmed_tokens = [stemmer.stem(token) for token in tokens]
-
     return " ".join(stemmed_tokens)
 
 # Pré-processa as perguntas e respostas
@@ -67,31 +66,52 @@ def chatbot_response(user_input):
 # Função de resposta padrão
 def default_response(user_input):
     return "\033[1;31mDesculpe, não entendi. Pode reformular a pergunta?\033[m"
+
 # Função que mostra as opções disponíveis
 def show_menu():
     print("\033[1;34mEscolha uma das opções abaixo:\033[m")
     print("\033[1m1 - Tirar dúvidas sobre IPO\033[m")
     print("\033[1m2 - Sumarizar o prospecto\033[m")
-    print("\033[1m3 - Sair\033[m")
+    print("\033[1m3 - Abrir pagina com prospectos\033[m")
+    print("\033[1m4 - Sair\033[m")
 
-# Função que trata a escolha do usuário
 # Função que trata a escolha do usuário
 def handle_choice(choice):
     if choice == "1":
         question = input("\033[1;32mDigite sua pergunta: \033[m")
         response = chatbot_response(question)
-        print(response)
+        wrapped_response = textwrap.wrap(response, width=50)  # Limite de 50 caracteres por linha
+        for line in wrapped_response:
+            print(line)
+        print()  # Pular linha adicional
     elif choice == "2":
         print("Em desenvolvimento")
     elif choice == "3":
+        open_google()
+    elif choice == "4":
         print("Até mais!")
         print("\033[1;34mPor favor, avalie o chatbot. Digite uma nota de 1 a 5 (sendo 1 a pior e 5 a melhor):\033[m")
         rating = input("\033[1;32mSua avaliação: \033[m")
-        # Aqui você pode fazer o processamento da avaliação, como armazenar em um banco de dados ou arquivo
-        print("Avaliação enviada. Obrigado!")
-        exit()
-    else:
+        if rating.isdigit() and 1 <= int(rating) <= 5:
+            # Aqui você pode fazer o processamento da avaliação, como armazenar em um banco de dados ou arquivo
+            print("Avaliação enviada. Obrigado!")
+            exit()
+        else:
+            print("\033[1;31mAvaliação inválida. Por favor, digite uma nota de 1 a 5.\033[m")
+    elif choice.isdigit() and 1 <= int(choice) <= 5:
+        print("Opção em desenvolvimento")
+    elif choice.isdigit():
         print("\033[1;31mOpção inválida. Por favor, tente novamente.\033[m")
+    else:
+        print("\033[1;31mEntrada inválida. Por favor, tente novamente.\033[m")
+def open_google():
+    # Abrir a página do Google
+    webbrowser.open('https://statusinvest.com.br/ipo/acoes')
+    # Voltar para o menu
+    show_menu()
+    choice = input("\033[1;32mDigite sua escolha: \033[m")
+    handle_choice(choice)
+
 # Loop principal do chatbot
 while True:
     show_menu()
--- Droid/sumarizacao.py
+++ Droid/sumarizacao.py
@@ -1,10 +1,5 @@
 import PyPDF2
 import re
-import regex as re
-import datetime
-import tkinter as tk
-from tkinter import messagebox
-from transformers import BartTokenizer, BartForConditionalGeneration
 
 # Ler o prospecto
 try:
@@ -18,57 +13,44 @@ except FileNotFoundError:
     exit()
 
 # Identificar as informações
-preco_oferta = re.findall(r'Preço\s+por\s+ação\s*:? *R?\$? *([\d,.]+)\s*', prospecto_text, re.IGNORECASE)
-quantidade_oferta = re.findall(r'Quantidade\s+de\s+ações\s+ofertadas\s*:? *([\d,.]+)\s*', prospecto_text, re.IGNORECASE)
-valor_total_oferta = re.findall(r'Valor\s+total\s+da\s+oferta\s*:? *R?\$? *([\d,.]+)\s*', prospecto_text, re.IGNORECASE)
-data_inicio_oferta = re.findall(r'Data\s+de\s+início\s+da\s+oferta\s*:? *([\d/]+)\s*', prospecto_text, re.IGNORECASE)
-data_fim_oferta = re.findall(r'Data\s+de\s+término\s+da\s+oferta\s*:? *([\d/]+)\s*', prospecto_text, re.IGNORECASE)
-objetivo_oferta = re.findall(r'Objetivo\s+da\s+oferta\s*:? *(.+?)\s*(?=Caracter|Riscos|Informa|\Z)', prospecto_text, re.DOTALL | re.IGNORECASE)
-garantias_oferta = re.findall(
-    r'Garantias\s+oferecidas\s+aos\s+investidores\s*:? *(.+?)\s*(?=Objetivo|Caracter|Riscos|Informa|\Z)',
-    prospecto_text, re.DOTALL | re.IGNORECASE)
-riscos_oferta = re.findall(
-    r'4\.1\s*-\s*Descrição\s*-\s*Fatores\s*de\s*Risco\s*:\s*(.*?)\s*(?=4\.2|\Z)', prospecto_text,
-    re.DOTALL | re.IGNORECASE)
+capital_social = re.findall(r'Capital\s+Social\s*:? *([\d,.]+)\s*', prospecto_text, re.IGNORECASE)
+data_liquidacao = re.findall(r'Data\s+de\s+Liquidação\s*:? *([\d/]+)\s*', prospecto_text, re.IGNORECASE)
+corretora = re.findall(r'Corretora\s*:? *(.+?)\s*(?=Aviso\s+ao\s+Mercado|\Z)', prospecto_text, re.DOTALL | re.IGNORECASE)
+aviso_ao_mercado = re.findall(r'Aviso\s+ao\s+Mercado\s*:? *(.+?)\s*(?=Capital\s+Social|Data\s+de\s+Liquidação|\Z)', prospecto_text, re.DOTALL | re.IGNORECASE)
 
-informacoes_oferta = re.findall(
-    r'Informações\s+sobre\s+a\s+empresa\s+e\s+a\s+oferta\s*:? *(.+?)\s*(?=Objetivo|Caracter|Garantias|Riscos|\Z)',
-    prospecto_text, re.DOTALL | re.IGNORECASE)
+# Salvar as informações em um arquivo de texto
+with open('informacoes.txt', 'w', encoding='utf-8') as txt_file:
+    if capital_social:
+        txt_file.write('**Capital Social:** R$' + capital_social[0].replace(',', '') + '\n')
+        txt_file.write('\n')
+    if data_liquidacao:
+        txt_file.write('**Data de Liquidação:** ' + data_liquidacao[0] + '\n')
+        txt_file.write('\n')
+    if corretora:
+        txt_file.write('**Corretora:** ' + corretora[0] + '\n')
+        txt_file.write('\n')
+    if aviso_ao_mercado:
+        aviso_resumido = aviso_ao_mercado[0][:100] + '...' if len(aviso_ao_mercado[0]) > 100 else aviso_ao_mercado[0]
+        txt_file.write('**Aviso ao Mercado:** ' + aviso_resumido + '\n')
+        txt_file.write('\n')
 
-# Sumarizar o prospecto
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+print('As informações foram salvas no arquivo informacoes.txt')
 
-summarized_text = ''
-if prospecto_text:
-    inputs = tokenizer([prospecto_text], truncation=True, max_length=1024, return_tensors='pt')
-    summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=100, early_stopping=True)
-    summarized_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+# Sumarizar as informações
+summary = ''
 
-# Criar arquivo de texto e salvar informações
-try:
-    with open('informacoes_prospecto.txt', 'w') as txt_file:
-        if preco_oferta:
-            txt_file.write('Preço por ação: R$ {}\n\n'.format(preco_oferta[0]))
-        if quantidade_oferta:
-            txt_file.write('Quantidade de ações ofertadas: {}\n\n'.format(quantidade_oferta[0]))
-        if valor_total_oferta:
-            txt_file.write('Valor total da oferta: R$ {}\n\n'.format(valor_total_oferta[0]))
-        if data_inicio_oferta:
-            txt_file.write('Data de início da oferta: {}\n\n'.format(data_inicio_oferta[0]))
-        if data_fim_oferta:
-            txt_file.write('Data de término da oferta: {}\n\n'.format(data_fim_oferta[0]))
-        if objetivo_oferta:
-            txt_file.write('Objetivo da oferta: {}\n\n'.format(objetivo_oferta[0].strip()))
-        if garantias_oferta:
-            txt_file.write('Garantias oferecidas aos investidores: {}\n\n'.format(garantias_oferta[0].strip()))
-        if riscos_oferta:
-            txt_file.write('Riscos envolvidos na oferta: {}\n\n'.format(riscos_oferta[0].strip()))
-        if informacoes_oferta:
-            txt_file.write('Informações sobre a empresa e a oferta: {}\n\n'.format(informacoes_oferta[0].strip()))
-        if summarized_text:
-            txt_file.write('Sumarização do prospecto: {}\n\n'.format(summarized_text.strip()))
-    messagebox.showinfo(title='Sucesso', message='As informações foram salvas no arquivo "informacoes_prospecto.txt"')
-except Exception as e:
-    messagebox.showerror(title='Erro', message='Ocorreu um erro ao criar o arquivo de texto: {}'.format(str(e)))
+if capital_social:
+    summary += f"**Capital Social:** R${capital_social[0].replace(',', '')[:50]}...\n\n"
 
+if data_liquidacao:
+    summary += f"**Data de Liquidação:** {data_liquidacao[0][:50]}...\n\n"
+
+if corretora:
+    summary += f"**Corretora:** {corretora[0][:50]}...\n\n"
+
+if aviso_ao_mercado:
+    aviso_resumido = aviso_ao_mercado[0][:100] + '...' if len(aviso_ao_mercado[0]) > 100 else aviso_ao_mercado[0]
+    summary += f"**Aviso ao Mercado:** {aviso_resumido[:50]}...\n\n"
+
+print("Resumo das informações:")
+print(summary)
