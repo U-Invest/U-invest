6e00b368b232e56680c505a86c157944a32382f0
matheusPierro <matpierro570@gmail.com>
2023-05-22 11:07:37 -0300

    Droid Completo com todas as features e estilizações

--- Droid/droid.py
+++ Droid/droid.py
@@ -9,6 +9,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.tokenize import sent_tokenize
 from heapq import nlargest
+import sumarizacao
+import os
 nltk.download('stopwords')
 nltk.download('punkt')   
 
@@ -21,11 +23,16 @@ with open('ipo.txt', 'r' ,encoding= "UTF8") as f:
 pairs = [pair.split('\n') for pair in raw_data.split('\n\n')]
 pairs = [(pair[0], pair[1]) for pair in pairs if len(pair) > 1]
 
-print("\033[1;34m" + "-" * 89)
-print("-" * 89 + "\033[m")
-print("\033[97;42mBem-vindo ao Droid! Eu sou um chatbot sobre IPOs. Como posso ajudá-lo?\033[m")
-print("\033[1;34m" + "-" * 89)
-print("-" * 89 + "\033[m")
+def limpaTerminal():
+    return os.system('cls' if os.name == 'nt' else 'clear')
+
+def criaBarra():
+    return print('-' * 32)
+
+limpaTerminal()
+criaBarra()
+print('\033[1;96m''Bem-vindo ao Droid! Eu sou um chatbot sobre IPOs. Como posso ajudá-lo?''\033[0;0m')
+criaBarra()
 
 def preprocess(text):
     tokens = nltk.word_tokenize(text.lower())
@@ -52,14 +59,14 @@ def chatbot_response(user_input):
             response = default_response(user_input)
         return response
     except Exception as e:
-        print(f"\033[1;31mErro ao processar a entrada do usuário: {e}\033[m")
+        print('\033[1;31m'f'Erro ao processar a entrada do usuário: {e}''\033[0; 0m')
         return None
     
 def default_response(user_input):
-    return "\033[1;31mDesculpe, não entendi. Pode reformular a pergunta?\033[m"
+    return '\033[1;31m''Desculpe, não entendi. Pode reformular a pergunta?''\033[0; 0m'
 
 def show_menu():
-    print("\033[1;34mEscolha uma das opções abaixo:\033[m")
+    print('\033[1;96m''Escolha uma das opções abaixo:''\033[0;0m')
     print("\033[1m1 - Tirar dúvidas sobre IPO\033[m")
     print("\033[1m2 - Sumarizar o prospecto\033[m")
     print("\033[1m3 - Abrir pagina com prospectos\033[m")
@@ -67,18 +74,24 @@ def show_menu():
 
 def handle_choice(choice):
     if choice == "1":
-        question = input("\033[1;32mDigite sua pergunta: \033[m")
+        limpaTerminal()
+        question = input('\033[1;96m''Digite sua dúvida: ''\033[0;0m')
         response = chatbot_response(question)
         wrapped_response = textwrap.wrap(response, width=50)
         for line in wrapped_response:
             print(line)
-        print()  
+        print()
     elif choice == "2":
-        print("Em desenvolvimento")
+        limpaTerminal()
+        caminho_pasta = "prospectos"
+        caminho_pasta_txt = "indices"
+        caminho_pasta_saida = "paginas"
+        sumarizacao.selecionar_arquivo(caminho_pasta, caminho_pasta_txt, caminho_pasta_saida)
     elif choice == "3":
+        limpaTerminal()
         open_google()
     elif choice == "4":
-        print("Até mais!")
+        print('\033[1;96m''Até mais!''\033[0;0m')
         avaliacoes = [] 
         try:
             with open("avaliacoes.json", "r") as file:
@@ -86,11 +99,11 @@ def handle_choice(choice):
         except FileNotFoundError:
             pass
         while True:
-            print("\033[1;34mPor favor, avalie o chatbot. Digite uma nota de 1 a 5 (sendo 1 a pior e 5 a melhor):\033[m")
+            print('\033[1;96m''Por favor, avalie o chatbot. Digite uma nota de 1 a 5 (sendo 1 a pior e 5 a melhor):''\033[0;0m')
             rating = input("\033[1;32mSua avaliação: \033[m")
             if rating.isdigit() and 1 <= int(rating) <= 5:
                 avaliacoes.append(int(rating))
-                print("\033[97;42mAvaliação enviada. Obrigado!\033[m")
+                print('Avaliação enviada. Obrigado!')
                 with open("avaliacoes.json", "w") as file:
                     json.dump(avaliacoes, file)
                 exit()
@@ -106,10 +119,10 @@ def handle_choice(choice):
 def open_google():
      webbrowser.open('https://statusinvest.com.br/ipo/acoes')
      show_menu()
-     choice = input("\033[1;32mDigite sua escolha: \033[m")
+     choice = input('\033[1;96m''Digite sua escolha: ''\033[0;0m')
      handle_choice(choice)
 
 while True:
     show_menu()
-    choice = input("\033[1;32mDigite sua escolha: \033[m")
+    choice = input('\033[1;96m''Digite sua escolha: ''\033[0;0m')
     handle_choice(choice)
--- Droid/sumarizacao.py
+++ Droid/sumarizacao.py
@@ -1,55 +1,215 @@
+import os
 import PyPDF2
 import re
 
-# Ler o prospecto
-try:
-    with open('prospecto.pdf', 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        prospecto_text = ''
-        for page in pdf_reader.pages:
-            prospecto_text += page.extract_text()
-except FileNotFoundError:
-    print('O arquivo do prospecto não foi encontrado')
-    exit()
+def limpaTerminal():
+    return os.system('cls' if os.name == 'nt' else 'clear')
 
-# Nomes possíveis
-nomes_possiveis = ['PRIVALIA BRASIL', 'CFL', 'AÇU PETRÓLEO', 'PRIVALIA BRASIL',
-                   "ENTALPIA PARTICIPAÇÕES", "YUNY", "EZ INC", "LIVETECH DA BAHIA INDÚSTRIA E COMÉRCIO",
-                   "NORTIS", "LABORATÓRIO TEUTO BRASILEIRO", "GRANBIO", "GRUPO MPR"]
+def criaBarra():
+    return print('-' * 32)
 
-# CNPJs possíveis
-cnpjs_possiveis = ['12345678000123', '98765432000198', '87654321000145']
+def extrair_pagina_indice(pdf_path, txt_path):
+    with open(pdf_path, 'rb') as arquivo_pdf:
+        leitor_pdf = PyPDF2.PdfReader(arquivo_pdf)
 
-# Identificar o nome da empresa
-nome_empresa_encontrado = None
-for nome in nomes_possiveis:
-    if re.search(r'\b' + re.escape(nome) + r'\b', prospecto_text, re.IGNORECASE):
-        nome_empresa_encontrado = nome
-        break
+        # Buscar pela página com o título "Índice"
+        pagina_indice = None
+        for num_pagina in range(len(leitor_pdf.pages)):
+            pagina = leitor_pdf.pages[num_pagina]
+            texto_pagina = pagina.extract_text()
+            if 'ÍNDICE' in texto_pagina:
+                pagina_indice = pagina
+                break
 
-# Identificar possíveis CNPJs
-cnpjs_encontrados = []
-for cnpj in cnpjs_possiveis:
-    if re.search(cnpj, prospecto_text):
-        cnpjs_encontrados.append(cnpj)
+        if pagina_indice is not None:
+            texto_indice = pagina_indice.extract_text()
 
-# Salvar o nome da empresa e os CNPJs encontrados em um arquivo de texto
-with open('nome_cnpj.txt', 'w', encoding='utf-8') as txt_file:
-    if nome_empresa_encontrado:
-        txt_file.write(f'Nome da empresa: {nome_empresa_encontrado}\n\n')
-    if cnpjs_encontrados:
-        txt_file.write('CNPJs encontrados:\n')
-        for cnpj in cnpjs_encontrados:
-            txt_file.write(cnpj + '\n')
+            # Salvar a página de índice em um arquivo TXT
+            with open(txt_path, 'w', encoding='utf-8') as arquivo_txt:
+                arquivo_txt.write(texto_indice)
 
-if nome_empresa_encontrado:
-    print(f'Nome da empresa: "{nome_empresa_encontrado}"')
-else:
-    print('Nenhum dos nomes possíveis foi encontrado no prospecto.')
+            # print(f'Página de índice extraída com sucesso! O arquivo "{pdf_path}" foi convertido para "{txt_path}".')
+        else:
+            print(f'Erro: Página de índice não encontrada no arquivo "{pdf_path}".')
 
-if cnpjs_encontrados:
-    print('CNPJs encontrados:')
-    for cnpj in cnpjs_encontrados:
-        print(cnpj)
-else:
-    print('Nenhum dos CNPJs possíveis foi encontrado no prospecto.')
+def buscar_descricao_oferta(txt_path):
+    with open(txt_path, 'r', encoding='utf-8') as arquivo_txt:
+        linhas = arquivo_txt.readlines()
+
+        for linha in linhas:
+            if 'descrição da oferta' in linha.lower():
+                # print(f'Linha encontrada: {linha.strip()}')
+                numeros = re.findall(r'\d+', linha)
+                if numeros:
+                    numero_final = int(numeros[-1])
+                    # print(f'Número encontrado: {numero_final}')
+                    return numero_final
+                break
+        else:
+            print('A palavra "descrição da oferta" não foi encontrada na página.')
+    return None
+
+def buscar_preco_acao(txt_path):
+    with open(txt_path, 'r', encoding='utf-8') as arquivo_txt:
+        linhas = arquivo_txt.readlines()
+
+        for linha in linhas:
+            if 'preço por ação' in linha.lower():
+                # print(f'Linha encontrada: {linha.strip()}')
+                numeros = re.findall(r'\d+', linha)
+                if numeros:
+                    numero_final = int(numeros[-1])
+                    # print(f'Número encontrado: {numero_final}')
+                    return numero_final
+                break
+        else:
+            print('A palavra "Preço por Ação" não foi encontrada na página.')
+    return None
+
+def buscar_publico_alvo(txt_path):
+    with open(txt_path, 'r', encoding='utf-8') as arquivo_txt:
+        linhas = arquivo_txt.readlines()
+
+        for linha in linhas:
+            if 'público alvo' in linha.lower():
+                # print(f'Linha encontrada: {linha.strip()}')
+                numeros = re.findall(r'\d+', linha)
+                if numeros:
+                    numero_final = int(numeros[-1])
+                    # print(f'Número encontrado: {numero_final}')
+                    return numero_final
+                break
+        else:
+            print('A palavra "Público Alvo" não foi encontrada na página.')
+    return None
+
+def buscar_pagina_por_numero(pdf_path, numero):
+    with open(pdf_path, 'rb') as arquivo_pdf:
+        leitor_pdf = PyPDF2.PdfReader(arquivo_pdf)
+
+        numero_pagina = numero + 4
+
+        if 1 <= numero_pagina <= len(leitor_pdf.pages):
+            pagina = leitor_pdf.pages[numero_pagina - 1]
+            texto_pagina = pagina.extract_text()
+            return texto_pagina
+        else:
+            print(f'Erro: Número da página {numero_pagina} fora do intervalo de páginas do PDF.')
+    return None
+
+def salvar_pagina_por_numero(pdf_path, numero, caminho_pasta_saida):
+    texto_pagina = buscar_pagina_por_numero(pdf_path, numero)
+    if texto_pagina:
+        nome_arquivo_saida = f"pagina_{numero + 4}.txt"
+        caminho_arquivo_saida = os.path.join(caminho_pasta_saida, nome_arquivo_saida)
+        with open(caminho_arquivo_saida, 'w', encoding='utf-8') as arquivo_saida:
+            arquivo_saida.write(texto_pagina)
+        # print(f'Página do PDF {pdf_path}, referente ao número {numero + 4}, salva em: {caminho_arquivo_saida}.')
+        return f"./paginas/{nome_arquivo_saida}"
+
+def encontrar_descricao_oferta(palavra_chave, nome_arquivo):
+    with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+        linhas = arquivo.readlines()
+
+        for i in range(len(linhas)):
+            linha = linhas[i].lower().strip()
+            if palavra_chave.lower() in linha:
+                proximo_paragrafo = ""
+                for j in range(i + 1, len(linhas)):
+                    proxima_linha = linhas[j].strip()
+                    if '.' in proxima_linha:
+                        ponto_index = proxima_linha.index('.')
+                        proximo_paragrafo += proxima_linha[:ponto_index + 1]
+                        break
+                    proximo_paragrafo += proxima_linha + " "
+                print(proximo_paragrafo.strip())
+                break
+
+def encontrar_preco_acao(palavra_chave, nome_arquivo):
+    with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+        linhas = arquivo.readlines()
+
+        for i in range(len(linhas)):
+            linha = linhas[i].lower().strip()
+            if palavra_chave.lower() in linha:
+                proximo_paragrafo = ""
+                for j in range(i + 1, len(linhas)):
+                    proxima_linha = linhas[j].strip()
+                    if '.' in proxima_linha:
+                        ponto_index = proxima_linha.index('.')
+                        proximo_paragrafo += proxima_linha[:ponto_index + 1]
+                        break
+                    proximo_paragrafo += proxima_linha + " "
+                print(proximo_paragrafo.strip())
+                break
+            
+def encontrar_publico_alvo(palavra_chave, nome_arquivo):
+    with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+        linhas = arquivo.readlines()
+
+        for i in range(len(linhas)):
+            linha = linhas[i].lower().strip()
+            if palavra_chave.lower() in linha:
+                proximo_paragrafo = ""
+                for j in range(i + 1, len(linhas)):
+                    proxima_linha = linhas[j].strip()
+                    if '.' in proxima_linha:
+                        ponto_index = proxima_linha.index('.')
+                        proximo_paragrafo += proxima_linha[:ponto_index + 1]
+                        break
+                    proximo_paragrafo += proxima_linha + " "
+                print(proximo_paragrafo.strip())
+                break
+
+def selecionar_arquivo(caminho_pasta, caminho_pasta_txt, caminho_pasta_saida):
+    nomes_arquivos = []
+    for nome_arquivo in os.listdir(caminho_pasta):
+        if os.path.isfile(os.path.join(caminho_pasta, nome_arquivo)):
+            nome_sem_extensao = os.path.splitext(nome_arquivo)[0]
+            nomes_arquivos.append(nome_sem_extensao)
+
+    print('\033[1;96m''Selecione sobre qual empresa você deseja abrir o prospecto para ler:''\033[0;0m')
+    for i, nome in enumerate(nomes_arquivos, start=1):
+        print('\033[1;96m'f'{i}.''\033[0;0m'f'{nome}')
+
+    escolha = input('\033[1;96m''Digite o número correspondente ao prospecto desejado: ''\033[0;0m')
+    escolha = int(escolha)
+
+    if 1 <= escolha <= len(nomes_arquivos):
+        nome_escolhido = nomes_arquivos[escolha - 1]
+        print('\033[1;32m'f'Você selecionou a empresa: {nome_escolhido}''\033[0; 0m')
+
+        pdf_path = os.path.join(caminho_pasta, f"{nome_escolhido}.pdf")
+        txt_path = os.path.join(caminho_pasta_txt, f"{nome_escolhido}_indice.txt")
+
+        extrair_pagina_indice(pdf_path, txt_path)
+        numeroDescricaoOferta = buscar_descricao_oferta(txt_path)
+        numeroPrecoAcao = buscar_preco_acao(txt_path)
+        numeroPulicoAlvo = buscar_publico_alvo(txt_path)
+        
+        if numeroDescricaoOferta is not None:
+            descOferta = salvar_pagina_por_numero(pdf_path, numeroDescricaoOferta, caminho_pasta_saida)
+            palavra_chave = 'Descrição da Oferta'
+            nome_arquivo = descOferta
+            print('\033[1;96m''Descrição da Oferta\n''\033[0;0m')
+            encontrar_descricao_oferta(palavra_chave, nome_arquivo)
+            criaBarra()
+            
+        if numeroPrecoAcao is not None:
+            print('\033[1;96m''Preço por Ação\n''\033[0;0m')
+            precoAcao = salvar_pagina_por_numero(pdf_path, numeroPrecoAcao, caminho_pasta_saida)
+            palavra_chave = 'Preço por Ação'
+            nome_arquivo = precoAcao
+            encontrar_preco_acao(palavra_chave, nome_arquivo)
+            criaBarra()
+            
+        if numeroPulicoAlvo is not None:
+            print('\033[1;96m''Público Alvo\n''\033[0;0m')
+            publicoAlvo = salvar_pagina_por_numero(pdf_path, numeroPulicoAlvo, caminho_pasta_saida)
+            palavra_chave = 'Público Alvo'
+            nome_arquivo = publicoAlvo
+            encontrar_publico_alvo(palavra_chave, nome_arquivo)
+            criaBarra()
+            
+    else:
+        print("Escolha inválida.")
